6370e5c8d964da6900f4c07bf9daca83359bf03b
Alex Vollmer <alex.vollmer@glutz..com>
2025-05-12 11:10:36 +0200

    sfu-frontend: new auth flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {IonApp, IonRouterOutlet, Platform} from '@ionic/angular/standalone';
-import {AuthService} from './shared/services/auth.service';
-import {SpotifyService} from './shared/services/spotify.service';
-import {Router} from '@angular/router';
+import {Component} from '@angular/core';
+import {IonApp, IonRouterOutlet} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
@@ -10,29 +7,5 @@ import {Router} from '@angular/router';
   standalone: true,
   imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent implements OnInit {
-
-  constructor(
-    private authService: AuthService,
-    private spotifyService: SpotifyService,
-    private readonly router: Router,
-    private platform: Platform
-  ) {
-  }
-
-  async ngOnInit() {
-    await this.platform.ready().then(() => {
-      setTimeout(() => {
-        if (this.router.url.startsWith('/callback')) return;
-
-        // 2)  Gewohnte Logik
-        if (this.authService.isLoggedIn()) {
-          this.authService.refresh();
-        } else {
-          this.authService.login();
-        }
-      }, 3000)
-    });
-  }
-
+export class AppComponent {
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,29 @@
-import {CallbackComponent} from './pages/callback/callback.component';
 import {Routes} from '@angular/router';
+import {CallbackComponent} from './pages/callback/callback.component';
+import {AuthGuard} from './shared/services/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '',
-    pathMatch: 'full',
-  },
-  {
-    path: '',
-    loadComponent: () => import('./pages/tab-artists/tab-artists.page').then(m => m.TabArtistsPage)
+    loadComponent: () =>
+      import('./pages/tab-artists/tab-artists.page').then(m => m.TabArtistsPage),
+    canActivate: [AuthGuard],
   },
   {
     path: 'tab-playlist',
-    loadComponent: () => import('./pages/tab-playlist/tab-playlist.page').then(m => m.TabPlaylistPage)
-  },
-  {
-    path: 'callback',
-    component: CallbackComponent
+    loadComponent: () =>
+      import('./pages/tab-playlist/tab-playlist.page').then(m => m.TabPlaylistPage),
+    canActivate: [AuthGuard],
   },
   {
     path: 'settings',
-    loadComponent: () => import('./pages/settings/settings.page').then(m => m.SettingsPage)
-  }
+    loadComponent: () =>
+      import('./pages/settings/settings.page').then(m => m.SettingsPage),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'callback',
+    component: CallbackComponent,
+  },
+  {path: '**', redirectTo: ''},
 ];
