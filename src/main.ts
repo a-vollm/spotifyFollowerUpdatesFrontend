d477f31d6a4d9ebf3da486f81887ee68ca5d0327
Alex Vollmer <alex.vollmer@glutz..com>
2025-05-12 15:36:32 +0200

    sfu-frontend:  interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,9 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import {PreloadAllModules, provideRouter, RouteReuseStrategy, withHashLocation, withPreloading} from '@angular/router';
 import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone';
-import {HTTP_INTERCEPTORS, provideHttpClient} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {isDevMode} from '@angular/core';
 import {provideServiceWorker} from '@angular/service-worker';
-
 import {AppComponent} from './app/app.component';
 import {routes} from './app/app.routes';
 import {TokenInterceptor} from './app/shared/services/auth.interceptor';
@@ -18,18 +17,17 @@ bootstrapApplication(AppComponent, {
       withHashLocation()
     ),
     {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+
+    // 👉  Interceptor per withInterceptors() registrieren
+    provideHttpClient(withInterceptorsFromDi()),
+    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true},
+
     provideIonicAngular({mode: 'md'}),
-    provideHttpClient(),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptor,
-      multi: true
-    },
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
     })
-  ],
+  ]
 });
 
 function setAppHeight() {
