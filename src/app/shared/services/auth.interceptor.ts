a1f7449fd73f67fa284fee5ff24ed6b8625d068b
Alex Vollmer <alex.vollmer@glutz..com>
2025-05-13 22:02:23 +0200

    sfu-frontend: refresh via backend

--- src/app/shared/services/auth.interceptor.ts
+++ src/app/shared/services/auth.interceptor.ts
@@ -1,13 +1,25 @@
 import {Injectable} from '@angular/core';
-import {HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+import {environment} from '../../../environments/environment';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const uid = localStorage.getItem('uid') ?? '';
     const cloned = req.clone({
       setHeaders: {'x-user-id': uid}
     });
-    return next.handle(cloned);
+
+    return next.handle(cloned).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401) {
+          localStorage.removeItem('uid');
+          window.location.href = `${environment.apiUrl}/auth/spotify`;
+        }
+        return throwError(() => err);
+      })
+    );
   }
 }
